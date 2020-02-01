a540b6e38892b23f650fc802171019acb4255857
admin <devqrx+admin@gmail.com>
2020-02-02 06:53:51 +0900

    Changed test file

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestFoo(t *testing.T) {
-	expect := "foo"
+	expect := "hello world"
 	actual := foo()
 	if actual != expect {
 		t.Errorf("\ngot : %v, want: %v\n", actual, expect)
